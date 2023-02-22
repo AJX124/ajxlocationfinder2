5b44bb83dc514a73529a3b1cec212977845a28dc
Ajay Gawade <ajay.gawade@atomtech.in>
2023-02-22 12:37:31 +0530

    Intial Commit

---
+++ ajxlocationfinder/src/main/java/com/ajx/ajxlocationfinder/Calculate.java
@@ -0,0 +1,15 @@
+package com.ajx.ajxlocationfinder;
+
+import android.location.Location;
+
+public class Calculate {
+
+    public static float getDistanceInKM(double startLatitude,double startLongitude,double endLatitude, double endLongitude){
+        System.out.println("In getDistanceInKM");
+        float[] result = new float[]{0};
+        Location.distanceBetween(startLatitude,startLongitude,endLatitude,endLongitude,result);
+
+        return result[0] / 1000;
+    }
+}
+
---
+++ app/src/main/java/com/ajx/testlocationfinder3/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ajx.testlocationfinder3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.ajx.ajxlocationfinder.Calculate;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Calculate cal;
+    TextView txtv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        float distance = cal.getDistanceInKM(18.9398, 19.1136, 72.8354, 72.8697);
+        System.out.println("Distance:"+distance);
+
+        // globally
+        txtv = (TextView)findViewById(R.id.txt1);
+        //in your OnCreate() method
+        txtv.setText(String.valueOf(distance));
+    }
+}
